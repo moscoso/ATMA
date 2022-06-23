55074d10ab08ebcdeb96a594fd3bc2097693430d
Chris Moscoso <Christophermoscoso@gmail.com>
2022-06-23 14:23:03 -0400

    Set production key

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,7 +2,7 @@
  * The publishable keys that enable the Stripe API
  */
 export const stripe = {
-    'production': 'pk_live_T6qB2lI9q51daLjTsDEX5tPX00Hzpq5PY3',
+    'production': 'pk_live_51KwdeaKq23fzvpkdUeb919IMzsXqLoG4Ann5LwhoMnhN9Vg14VXWvBInTQ8BiZfdVhKwogPEytaI5pTmawfQFHpA00y45TgEl8',
     'staging': 'pk_test_51GuLd0LM5gy7nmLDEXT328ttXY6al7FlGMHFdMjeouQA5ueFc2AENeiESgOjpoEoulTB7gnPHRLElLxS2Jz4IXea00VOHd9qXF'
 };
 
